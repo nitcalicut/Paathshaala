77ed3b04f408af86bf8784d5552e4c1432db2a68
Vipin Nair <swvist@gmail.com>
2012-02-01 21:40:58 +0530

    Added checks and returning cid on success

--- response/submitvideo.php
+++ response/submitvideo.php
@@ -1,12 +1,27 @@
 <?php
 
-	
 	include '../functions/class.video.php';
+	session_start();
+	global $global_raw_videos_folder, ;
 	global $global_user_folder;
-	$v = new video($_POST['title'],$_POST['desc'],$_SESSION['uid'],"SP",$global_user_folder,$_POST['file']);
-	if($v->getContentId()){
-		$v->addTags($_POST['tags']);
-		echo "{ \"status\" : 1 }";
+	if(	isset($_POST['title']) &&
+		isset($_POST['desc']) &&
+		isset($_SESSION['uid']) &&
+		isset($_POST['file']) &&
+		isset($_POST['tags'])){
+			$v = new video($_POST['title'],
+							$_POST['desc'],
+							$_SESSION['uid'],
+							"SP",
+							$global_user_folder,
+							$_POST['file']);
+			if($v->getContentId()){
+				$v->addTags($_POST['tags']);
+				echo "{ \"status\" : 1 ,\"cid\" : ".$v->getContentId()."}";
+			}
+			else{
+				echo "{ \"status\" : 0 }";
+			}
 	}
 	else{
 		echo "{ \"status\" : 0 }";
