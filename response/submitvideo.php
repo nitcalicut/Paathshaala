3653332113c0c19815e98165735651dff35f9db1
Jaseem Abid <jaseemabid@gmail.com>
2012-02-01 21:52:52 +0530

    fixing session bug while submitting the video

--- response/submitvideo.php
+++ response/submitvideo.php
@@ -1,13 +1,32 @@
 <?php
-	session_start();
+
 	include '../functions/class.video.php';
-	global $global_user_folder;
-	$v = new video($_POST['title'],$_POST['desc'],$_SESSION['uid'],"SP",$global_user_folder,$_POST['file']);
-	if($v->getContentId()){
-		$v->addTags($_POST['tags']);
-		echo "{ \"status\" : 1 }";
+	session_start();
+	global $global_raw_videos_folder, $global_user_folder;
+	if(	isset($_POST['title']) &&
+		isset($_POST['desc']) &&
+		isset($_SESSION['uid']) &&
+		isset($_POST['file']) &&
+		isset($_POST['tags'])){
+			$v = new video($_POST['title'],
+							$_POST['desc'],
+							$_SESSION['uid'],
+							"SP",
+							$global_user_folder,
+							$_POST['file']);
+			if($v->getContentId()){
+				$v->addTags($_POST['tags']);
+				echo "{ \"status\" : 1 ,\"cid\" : ".$v->getContentId()."}";
+			}
+			else{
+				echo "{ \"status\" : 0 }";
+			}
 	}
 	else{
 		echo "{ \"status\" : 0 }";
 	}
+
+	
+
+
 ?>
